e310ca9d102379968ccdd44d866a9ffa26689db7
Luba-web <ldvasileva87@yandex.ru>
2023-07-31 19:30:49 +0300

    fix delete console.log and add https

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,14 @@
 const getPolyline = async (points) => {
   try {
     const pointsStr = points.map((i) => i.slice().reverse()).join(';');
-    // console.log('pointsStr', pointsStr);
     const response = await fetch(
-      `http://router.project-osrm.org/route/v1/driving/${pointsStr}?steps=true&geometries=geojson`
+      `https://router.project-osrm.org/route/v1/driving/${pointsStr}?steps=true&geometries=geojson`
     );
     if (response.ok) {
       let json = await response.json();
       let res = json.routes[0].geometry.coordinates.map((i) =>
         i.slice().reverse()
       );
-      // console.log('res', res);
       return res;
     }
     throw Error(response.statusText);
@@ -18,7 +16,5 @@ const getPolyline = async (points) => {
     console.log(error);
   }
 };
+
 export default getPolyline;
-// export const getRouterApi = () => {
-//   // запрос за маршрутами
-// };
